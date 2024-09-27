d5021a5ed613b1ff5aa6c5a3209dc0ce780d1371
Michel Monteiro Schorr <mschorrmichel@gmail.com>
2024-09-26 22:01:11 -0300

    q1 completa, tratamento basico de erros adicionado

--- api/app/app.js
+++ api/app/app.js
@@ -15,50 +15,56 @@ app.get('/', (req, res) =>{
 
 app.post('/age', (req, res) => {
 
+    //recupera o body
     const data = req.body;
 
-    console.log(data);
-    const name = data.name;
-    const birthdate = new Date(data.birthdate);
-    const date = new Date(data.date);
+    //checando completude do body
+    if(!data.name || !data.date || !data.birthdate){
+        return res.status(400).json({message: 'Atributos faltando ou inválidos. Requests devem ser no formato: {"name": "Nome Sobrenome","birthdate": "yyyy-mm-dd ","date":  "YYYY-MM-DD "}'});
+    }
 
-    console.log(name, birthdate, date);
 
+    //converte as data para o formato Date
+    data.date = new Date(data.date);
+    data.birthdate = new Date(data.birthdate);
+
+    //checando validade do date
+    if(data.date<data.birthdate){
+        return res.status(400).json({message: 'Atributo date inválido. date deve ser uma data mais recente que birthdate'});
+    }
+
+ 
+
+    //data atual
     const today = new Date();
+
+
+    //ageNow
+    var ageNow;
+    if((today.getMonth()>data.birthdate.getMonth())||((today.getMonth()==data.birthdate.getMonth())&&(today.getDate()>=data.birthdate.getDate()))){
+        ageNow = today.getFullYear() - data.birthdate.getFullYear();
+    }else{
+        ageNow = today.getFullYear() - data.birthdate.getFullYear() - 1;
+    }
+
+    //ageThen
+    var ageThen;
+    if((data.date.getMonth()>data.birthdate.getMonth())||((data.date.getMonth()==data.birthdate.getMonth())&&(data.date.getDate()>=data.birthdate.getDate()))){
+        ageThen = data.date.getFullYear() - data.birthdate.getFullYear();
+    }else{
+        ageThen = data.date.getFullYear() - data.birthdate.getFullYear() - 1;
+    }
+
+
     
-
-
-
-    var age_today;
-    //age_today
-    if((today.getMonth()>birthdate.getMonth())||((today.getMonth()==birthdate.getMonth())&&(today.getDate()>=birthdate.getDate()))){
-        age_today = today.getFullYear() - birthdate.getFullYear();
-    }else{
-        age_today = today.getFullYear() - birthdate.getFullYear() - 1;
-    }
-
-    var age_then;
-    //age_then
-    if((date.getMonth()>birthdate.getMonth())||((date.getMonth()==birthdate.getMonth())&&(date.getDate()>=birthdate.getDate()))){
-        age_then = date.getFullYear() - birthdate.getFullYear();
-    }else{
-        age_then = date.getFullYear() - birthdate.getFullYear() - 1;
-    }
-
-
+    //resposta
     const response = 
     {
-        quote: `Olá, ${name}! Você tem ${age_today} anos e em ${(date.getDate()+1).toString().padStart(2, '0')}/${(date.getMonth()+1).toString().padStart(2, '0')}/${date.getFullYear()} você tería ${age_then} anos.`,
-        ageNow: age_today,
-        ageThen: age_then
+        quote: `Olá, ${data.name}! Você tem ${ageNow} anos e em ${(data.date.getDate()+1).toString().padStart(2, '0')}/${(data.date.getMonth()+1).toString().padStart(2, '0')}/${data.date.getFullYear()} você tería ${ageThen} anos.`,
+        ageNow: ageNow,
+        ageThen: ageThen
     }
-        
-
     res.status(200).json(response);
-
-
-    res.status(200).json(data);
-
 })
 
 
